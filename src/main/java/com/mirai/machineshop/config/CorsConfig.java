5ebe654daf4b9e39529b859f4fc95628667d6f64
pranav <pranavk346@gmail.com>
2026-08-28 11:54:37 +0530

    Fix backend database configuration

--- src/main/java/com/mirai/machineshop/config/CorsConfig.java
+++ src/main/java/com/mirai/machineshop/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig implements WebMvcConfigurer {
                 .allowedOrigins(
                         "http://localhost:5173",
                         "http://localhost:5174",
-                        "http://localhost:5175"
+                        "http://localhost:5176"
                 )
                 .allowedMethods(
                         "GET",
